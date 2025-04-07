cd6c90469b5b2ad80c7cab0b7a7e75035871c115
Igor Kuruc <krc13@email.cz>
2025-04-07 16:09:38 +0200

    add game loop

--- src/application/GameOfLife.java
+++ src/application/GameOfLife.java
@@ -3,6 +3,8 @@ package application;
 import java.util.HashSet;
 import java.util.Set;
 
+
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.input.KeyCode;
@@ -14,6 +16,10 @@ import javafx.stage.Stage;
 
 public class GameOfLife extends Application {
 	
+    private long lastUpdateTime = 0;
+    private final double FPS = 2;  // Target FPS
+    private final long NANOS_PER_UPDATE = 1_000_000_000 / (long) FPS;  // Time per frame in nanoseconds
+
 	int width = 800;
 	int height = 800;
 	int rectangleSize = 50;
@@ -22,8 +28,7 @@ public class GameOfLife extends Application {
 	
 	Set<Rectangle> liveCells = new HashSet<>();
 	
-	public void clickCells(MouseEvent event,Set<Rectangle> liveCells, Pane pane) {
-		
+	public void clickCells(MouseEvent event,Set<Rectangle> liveCells, Pane pane) {		
         // Get click coordinates
         double x = event.getX();
         double y = event.getY();
@@ -49,19 +54,43 @@ public class GameOfLife extends Application {
 	}
 	
     @Override
-    public void start(Stage primaryStage) {
-    	
+    public void start(Stage primaryStage) {  	
         // Create a pane
         Pane pane = new Pane();
         //create Scene
         Scene scene = new Scene(pane, width, height);
-        
-        //NULL GENERATION
-        
-        // Adding live cells from user input
+      
+        // Adding and removing live cells from user input
         pane.setOnMouseClicked((MouseEvent event) -> {
         		clickCells(event, liveCells, pane);
         });
+        
+        //start the game by pressing SPACE
+        scene.setOnKeyPressed(e -> {
+            if( e.getCode() == KeyCode.SPACE) {
+            	System.out.println("ENTER PRESSED; disable user input");
+            	allowUserInput = false;
+            }
+        });
+        
+        // Game loop with fixed FPS using AnimationTimer
+        AnimationTimer gameLoop = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                if (now - lastUpdateTime >= NANOS_PER_UPDATE) {
+                	
+                    // Update the game state and render the new frame
+                	if (!allowUserInput) {      		
+                		
+                	}
+                    // Update last update time
+                    lastUpdateTime = now;
+                }
+            }
+        };
+                
+        // Start the game loop
+        gameLoop.start();
         
         primaryStage.setTitle("Game of Life");
         primaryStage.setScene(scene);
