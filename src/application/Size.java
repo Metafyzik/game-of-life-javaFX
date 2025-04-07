fbbf05369ee02ecaf65e45a58c81749431a8ed09
Igor Kuruc <krc13@email.cz>
2025-04-07 20:25:38 +0200

    add constants class
    Separate class containing general values about sizes of the window and
    rectagles (cells).

---
+++ src/application/Size.java
@@ -0,0 +1,11 @@
+package application;
+
+public final class Size {
+	private Size ( ) {};
+	
+	public static final int WINDOW_WIDTH = 800;
+	public static final int WINDOW_HEIGHT = 800;
+
+	public static final int CELL_RECT_SIDE = 25;
+	public static final int CELL_RECT_ARC = 10;
+}
